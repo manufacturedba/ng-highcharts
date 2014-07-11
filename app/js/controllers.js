ef9b2eb6159e641b6e45398da4856f8a57f66c38
manufacturedba <manufacturedba@gmail.com>
2014-07-11 15:30:09 -0500

    More fun

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,5 +4,27 @@
 
 angular.module('demoApp.controllers', [])
   .controller('demoCtrl', ['$scope', function($scope) {
-	  $scope.test = {}
+      $scope.test = {
+          title: {
+              text: "Hi friend"
+          },
+          series: [
+              {
+                name: 'hi',
+                data: [1,2,3]
+              },
+              {
+                data: [3,2,1]
+              }
+          ]
+      }
+      $scope.removeSeries = function(){
+          $scope.test.series.pop();
+      }
+      $scope.addSeries = function(){
+        $scope.test.series.push({'name': 'shuddup', data:[5,4,3]})
+      }
+      $scope.changeSeries = function(){
+          $scope.test.series[0].data[0] = Math.ceil(Math.random()*100);
+      }
   }]);
--- app/js/highChartDir.js
+++ app/js/highChartDir.js
@@ -3,7 +3,15 @@
 /* Directives */
 
 var directiveController = ['$scope', function(scope){
-
+    // Watch title
+    scope.$watch(function(){
+            return scope.options.title;
+        }, function(value, oldValue){
+            if(!angular.equals(value, oldValue)){
+                scope.chart.setTitle(value);
+            }
+        }, true);
+    
 }];
 
 angular.module('demoApp.directives', []).
@@ -14,7 +22,6 @@ angular.module('demoApp.directives', []).
                         scope: {
                             options: '=options'
                         },
-                        controller: directiveController,
                         link: function(scope, elem){
                             if(typeof(scope.options) === 'object'){
                                 scope.options.chart = {
@@ -27,8 +34,69 @@ angular.module('demoApp.directives', []).
                                     }
                                 };
                             }
+
+                            // Create IDs
+                            var series = [];
+                            var id = 0;
+                            angular.forEach(scope.options.series, function(set){
+                                if(set.id === undefined){
+                                    set.id = id;
+                                    series.push(set.id);
+                                    id++;
+                                } else {
+                                    series.push(set.id);
+                                }
+                            });
+                            
                             scope.chart = new Highcharts.Chart(scope.options);
-                        }
+                            
+                            scope.$watch(function(){
+                                return scope.options.series;
+                            }, function(value, oldValue){
+                                for(var i = 0;i < value.length;i++){
+                                    if(!angular.equals(value[i].data, oldValue[i].data)){
+                                        scope.chart.get(value[i].id).setData(value[i].data);
+                                    }
+                                }
+                            }, true);
+
+                            // Series Add/Remove
+                            scope.$watchCollection(function(){
+                                return scope.options.series;
+                            }, function(values, oldValues){
+                                // Remove
+                                if(values.length < oldValues.length){
+                                    console.log('hi mum');
+                                    var checkSeries = [];
+                                    angular.forEach(values, function(set){
+                                        checkSeries.push(set.id);
+                                    });
+                                    angular.forEach(series, function(id){
+                                        if(checkSeries.indexOf(id) < 0){
+                                            scope.chart.get(id).remove(false);
+                                            series.splice(series.indexOf(id), 1);
+                                        }
+                                    });
+                                } else if(values.length > oldValues.length){
+                                    angular.forEach(values, function(set){
+                                        if(set.id === undefined){
+                                            set.id = id;
+                                            id++;
+                                        }
+                                        if(series.indexOf(set.id) < 0){
+                                            scope.chart.addSeries(set)
+                                            series.push(set.id);
+                                        }
+                                    });
+                                } else if(!angular.equals(values, oldValues)){
+                                    console.log('hi');
+                                    scope.chart.setData(values, false);
+                                }
+                                scope.chart.redraw();
+                            });
+                            window.chart = scope.chart;
+                        },
+                        controller: directiveController
                 }
         } else {
             return {
